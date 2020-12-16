0dcf246a50feb4ce0255a86f4798e60ee3de0f86
ANDRERAIMUNDO <9000andre@gmail.com>
2020-12-16 11:28:30 -0300

    Criação do projeto

---
+++ src/test/java/com/andre/mc/McApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
