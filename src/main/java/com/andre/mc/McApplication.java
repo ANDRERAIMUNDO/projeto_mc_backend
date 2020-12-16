2259df2c9885b1159a34fc43d6374b5fae28dae4
ANDRERAIMUNDO <9000andre@gmail.com>
2020-12-16 15:49:36 -0300

    Crianção instanciação no bando h2 McApplication

--- src/main/java/com/andre/mc/McApplication.java
+++ src/main/java/com/andre/mc/McApplication.java
@@ -1,13 +1,28 @@
 package com.andre.mc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class McApplication {
+import com.andre.mc.domain.Categoria;
+import com.andre.mc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class McApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
