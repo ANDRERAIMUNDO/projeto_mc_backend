04dfcc43eea728999ffe15d32d233d1a53990a80
ANDRERAIMUNDO <9000andre@gmail.com>
2020-12-16 15:36:43 -0300

    Criando classes de Repository e Service para Categoria

---
+++ src/main/java/com/andre/mc/services/CategoriaService.java
@@ -0,0 +1,18 @@
+package com.andre.mc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import com.andre.mc.domain.Categoria;
+import com.andre.mc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria buscar(Integer id) {
+		Optional <Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
