6872c3a3b79426e26fec73a476b30a93275e45f2
ANDRERAIMUNDO <9000andre@gmail.com>
2020-12-18 13:24:38 -0300

    Atualização de serialização Json , deletando JsonManegerReference e JsonBackReference e usando somente JsonIgnore

--- src/main/java/com/andre/mc/domain/Categoria.java
+++ src/main/java/com/andre/mc/domain/Categoria.java
@@ -8,8 +8,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
 
@@ -24,7 +22,6 @@ public class Categoria implements Serializable {
 	private Integer id ;
 	private String nome;
 	
-	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
     private List<Produto> produtos = new ArrayList<>();
 	public Categoria() {
