494125a610423621b5a960d8f9302ce8154b2988
ANDRERAIMUNDO <9000andre@gmail.com>
2021-01-05 10:57:16 -0300

    SmtpEmailService

--- src/main/java/com/andre/mc/config/DevConfig.java
+++ src/main/java/com/andre/mc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.andre.mc.services.DBService;
+import com.andre.mc.services.EmailService;
+import com.andre.mc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -26,5 +28,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
