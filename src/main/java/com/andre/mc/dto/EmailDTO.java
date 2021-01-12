c73aca5562ec87a14f4f9e0509c3fb71acbe7181
ANDRERAIMUNDO <9000andre@gmail.com>
2021-01-12 11:39:31 -0300

    POST auth/forgot esqueci a senha, enviando por email

---
+++ src/main/java/com/andre/mc/dto/EmailDTO.java
@@ -0,0 +1,26 @@
+package com.andre.mc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty(message="Email é campo obrigatorio. ")
+	@Email(message="Email em formato invalido. ")
+	private String email;
+	
+	public EmailDTO() {
+		
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
