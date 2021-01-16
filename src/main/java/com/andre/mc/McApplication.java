9099d63baf4324c651c011798cfe26d5fddab533
ANDRERAIMUNDO <9000andre@gmail.com>
2021-01-16 10:11:39 -0300

    Teste para salvar arquivo no AWS S3

--- src/main/java/com/andre/mc/McApplication.java
+++ src/main/java/com/andre/mc/McApplication.java
@@ -1,17 +1,24 @@
 package com.andre.mc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andre.mc.services.S3Service;
+
 @SpringBootApplication
 public class McApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\teste.jpg");
 	}
 }
