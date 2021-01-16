3c9fbda6fa20976b8ea9e5dbca5684df4c391beb
ANDRERAIMUNDO <9000andre@gmail.com>
2021-01-16 14:29:57 -0300

    Ajuste automatico imagem para 200px

--- src/main/java/com/andre/mc/services/ClienteService.java
+++ src/main/java/com/andre/mc/services/ClienteService.java
@@ -49,6 +49,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	@Autowired
 	private ImageService imageService; 
 	
@@ -135,10 +138,13 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
-		BufferedImage JpgImage = imageService.getJpgImageFromFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
-		return s3Service.uploadFile(imageService.getInputStream(JpgImage, "jpg"), fileName, "iamge");
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "iamge");
 	}
 }
 
--- src/main/java/com/andre/mc/services/ImageService.java
+++ src/main/java/com/andre/mc/services/ImageService.java
@@ -9,6 +9,8 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
+import org.imgscalr.Scalr;
+
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,38 +21,48 @@ import com.andre.mc.services.exception.FileException;
 public class ImageService {
 
 	public BufferedImage getJpgImageFromFile(MultipartFile uplodaFile) {
-		
+
 		String ext = FilenameUtils.getExtension(uplodaFile.getOriginalFilename());
-		
-		if(!"png".equals(ext) && !"jpg".equals(ext)) {
+
+		if (!"png".equals(ext) && !"jpg".equals(ext)) {
 			throw new FileException("Permitido somente imagem em formato PNG ou JPG. ");
 		}
 		try {
 			BufferedImage img = ImageIO.read(uplodaFile.getInputStream());
-			if("png".equals(ext)) {
+			if ("png".equals(ext)) {
 				img = pngToJpg(img);
 			}
 			return img;
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
-		
+
 	}
 
 	public BufferedImage pngToJpg(BufferedImage img) {
-		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(), 
-				BufferedImage.TYPE_INT_RGB);
+		BufferedImage jpgImage = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
 		jpgImage.createGraphics().drawImage(img, 0, 0, Color.WHITE, null);
 		return jpgImage;
 	}
-	
-	public InputStream getInputStream (BufferedImage img, String extension) {
+
+	public InputStream getInputStream(BufferedImage img, String extension) {
 		try {
-			ByteArrayOutputStream os = new	ByteArrayOutputStream();
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
 			ImageIO.write(img, extension, os);
 			return new ByteArrayInputStream(os.toByteArray());
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo");
 		}
+	}
+
+	// redimensionar imagem
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(sourceImg, (sourceImg.getWidth() / 2) - (min / 2), (sourceImg.getHeight() / 2) - (min / 2),
+				min, min);
+	}	
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
 	}
 }
